1899d9db8a0971f096acd511c57d756266a05996
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2021-03-03 09:50:55 +0000

    New translations tightrope-finished-main.py (Dutch)

--- nl-NL/resources/tightrope-finished-main.py
+++ nl-NL/resources/tightrope-finished-main.py
@@ -9,10 +9,10 @@ y = [255, 255, 0]
 x = [0, 0, 0]
 b = [0, 0, 255]
 
-charx = 0
-chary = 0
+persx = 0
+persy = 0
 
-path = [
+pad = [
   y,y,y,x,x,x,x,x,
   x,x,y,x,x,x,x,x,
   x,x,y,x,x,x,x,x,
@@ -24,30 +24,30 @@ path = [
 ]
 
 while True:
-  sense.set_pixels(path)
-  sense.set_pixel(charx, chary, b)
+  sense.set_pixels(pad)
+  sense.set_pixel(persx, persy, b)
 
-  pitch = sense.get_orientation()['pitch']
-  roll = sense.get_orientation()['roll']
+  helling = sense.get_orientation()['pitch']
+  kanteling = sense.get_orientation()['roll']
 
-  print('Pitch:', pitch)
-  print('Roll:', roll)
+  print('Helling:', helling)
+  print('Kanteling:', kanteling)
 
-  if 270 < pitch < 315 and charx < 7:
-    charx += 1
+  if 270 < helling < 315 and persx < 7:
+    persx += 1
 
-  if 45 < pitch < 90 and charx > 0:
-    charx -= 1
+  if 45 < helling < 90 and persx > 0:
+    persx -= 1
 
-  if 45 < roll < 90 and chary < 7:
-    chary += 1
+  if 45 < kanteling < 90 and persy < 7:
+    persy += 1
 
-  if 270 < roll < 315 and chary > 0:
-    chary -= 1
+  if 270 < kanteling < 315 and persy > 0:
+    persy -= 1
 
-  current = sense.get_pixel(charx, chary)
-  if current == x:
-    charx = 0
-    chary = 0
+  huidige = sense.get_pixel(persx, persy)
+  if huidige == x:
+    persx = 0
+    persy = 0
 
   sleep(0.4)
